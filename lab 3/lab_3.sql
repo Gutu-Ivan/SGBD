def252df7ff7e94c0fae2bd98fe5601c66214768
Gutu Ivan <54944261+Gutu-Ivan@users.noreply.github.com>
2022-03-28 14:07:24 +0300

    Update lab_3.sql

--- lab 3/lab_3.sql
+++ lab 3/lab_3.sql
@@ -1,7 +1,7 @@
 #1, 2
 create table articole(
 idArticol smallint primary key,
-denArticool varchar(100)
+denArticol varchar(100)
 );
 
 create table universitate (
@@ -12,46 +12,69 @@ denUniversitate varchar(100)
 create table cercetatori(
 idCercetator smallint,
 numeCercetator varchar(100),
-foreign key (idUniversitate) references universitate(idUniversitate)
+idUniversitate smallint
 );
 
-craete table autori(
+create table autori(
 idCercetator smallint,
 idArticol smallint,
-foreign key (idCercetator) references cercetatori(idCercetator),
 primary key (idArticol)
 );
+
+alter table autori
+    add constraint idCercetator
+    	foreign key (idCercetator) references cercetatori(idCercetator);
+	
+alter table cercetatori
+    add constraint idUniversitate
+        foreign key (idUniversitate) references universitate (idUniversitate);
+ALTER TABLE cercetatori ENGINE=InnoDB;
 
 #3
 insert into articole
 values  (1, 'Articol1'),
-	(2, 'Articol2'),
-	(3, 'Articol3'),
-	(4, 'Articol4'),
-	(5, 'Articol5');
+	    (2, 'Articol2'),
+	    (3, 'Articol3'),
+	    (4, 'Articol4'),
+	    (5, 'Articol5');
 
-insert into universitate (
+insert into universitate
 values  (1, 'USARB'),
-	(2, 'USM'),
-	(3, 'ASEM');
+	    (2, 'USM'),
+	    (3, 'ASEM');
 
 insert into cercetatori
 values  (1, 'Dodu Petru', 1),
-	(2, 'Lungu Vasile', 2),
-	(3, 'Vrabie Maria', 1),
-	(4, 'Ombun Bogdan', 3);
+	    (2, 'Lungu Vasile', 2),
+	    (3, 'Vrabie Maria', 1),
+	    (4, 'Ombun Bogdan', 3);
 
 insert into autori
 values  (1, 1),
-	(2, 2),
-	(3, 3),
-	(4, 4);
+	    (2, 2),
+	    (3, 3),
+	    (4, 4);
 
 #4
-#4.1
-create procedure pr1(idCercetator)
-	returns denArticol as $$
+create procedure pr1
+    (
+    idCercetator smallint
+)
 begin
-	select 1
-	order by denArticol desc;
+	select denArticol
+	from articole
+	order by idCercetator desc;
 end;
+
+#5
+create procedure pr2
+    (
+    idUniversitate smallint
+    )
+begin
+    select numeCercetator,denArticol
+    from cercetatori
+    inner join autori a1 on a1.idCercetator = cercetatori.idCercetator
+    inner join articole a on a.idArticol = a1.idArticol
+    where idUniversitate = 1;
+end; 
